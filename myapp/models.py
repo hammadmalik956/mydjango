95ee72ce00cfa9998ea0bf7fd1513a4aca17afac
Malik Hammad Hameed <hammadhameed956@gmail.com>
2023-11-21 02:20:57 +0500

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -13,12 +13,4 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    pub_date = models.DateTimeField('date published')
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category)
 
-    def __str__(self):
-        return self.title
