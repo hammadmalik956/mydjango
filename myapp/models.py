a3436ea0b8412f7e1278879e918a6d9b71fe30b7
Malik Hammad Hameed <hammadhameed956@gmail.com>
2023-11-21 01:53:45 +0500

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+rom django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
